d872fe7a1a6a7dac88e1f201d622d4339e8146fe
Pyrees011 <raunak.bhansali17@gmail.com>
2024-09-27 22:16:45 +0200

    feat: core components and utils

--- views/components/sidebar.js
+++ views/components/sidebar.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import Image from 'next/image'
+
+import { useRouter } from 'next/router'
 
 // icon
 import { Ellipsis } from 'lucide-react'
@@ -7,25 +9,61 @@ import { Settings } from 'lucide-react'
 import { SunMoon } from 'lucide-react';
 import { FileQuestion } from 'lucide-react';
 
-// component
+// shadecn
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
-const SidebarButton = (props) => {
-    return (
-        <div className="w-full h-14 flex items-center justify-left bg-transparent border-none px-6 mb-2 hover:bg-gray-900">
-            <Image src={props.icon} alt='image' width={32} height={32} className='mr-4'/>
-            <p className='text-xl text-white font-semibold'>{props.title}</p>
+
+
+export default function Sidebar() {
+    const router = useRouter()
+    const [ isAuth, setIsAuth ] = useState(true)
+
+  return (
+    <div className='bg-gray-100 w-[200px] h-full flex flex-col text-gray-800 sticky top-0'>
+      <div className='w-full h-12 flex items-center justify-left bg-transparent border-none p-4 mt-4 mb-2'>
+            <Image src={'/chess.png'} alt='image' width={32} height={32} className='relative z-10'/>
+            <p className='text-3xl font-bold absolute left-9 top-5'>GAMBITAI</p>
         </div>
-    )
+
+        {sidebarInfo.map((item, index) => (
+            <SidebarButton key={index} title={item.title} icon={item.icon}/>
+        ))}
+
+      <div className="w-full h-14 flex items-center justify-left bg-transparent border-none px-6 mb-2 hover:bg-slate-200">
+        <Ellipsis size={32} color='black' className='mr-4'/>
+        <p className='text-xl font-normal'>more</p>
+      </div>
+
+        <div className='px-4 mb-4'>
+            <Input type="text" placeholder="Search" className="h-10 border-black placeholder:text-black"/>
+        </div>
+
+      { !isAuth ? (
+        <div className='px-4 mb-4'>
+          <Button className="w-full bg-slate-500 h-12 text-md font-bold mb-4 hover:bg-gray-700" onClick={() => router.push('/register')}>Register</Button>
+          <Button className="w-full bg-gray-400 h-12 text-md font-bold hover:bg-gray-700" onClick={() => router.push('/login')}>Login</Button>
+        </div>
+      ) : null}
+
+      <div className='mt-auto mb-4'>
+        {sidebarFooterInfo.map((item, index) => (
+          <Button key={index} className="w-full bg-transparent flex justify-start items-center px-6 shadow-none hover:bg-transparent">
+            {item.icon}
+            <p className='text-md font-semibold text-gray-700'>{item.title}</p>
+          </Button>
+        ))}
+      </div>
+    </div>
+  )
 }
 
-const sidebarFooter = (props) => {
+const SidebarButton = (props) => {
     return (
-        <Button className="w-full bg-transparent flex justify-start	items-center px-6 hover:bg-transparent">
-                {props.icon}
-                <p className='text-white text-md font-semibold'>{ props.title }</p>
-            </Button>
+        <div className="w-full h-14 flex items-center justify-left bg-transparent border-none px-6 mb-2 hover:bg-slate-200">
+            <Image src={props.icon} alt='image' width={32} height={32} className='mr-4'/>
+            <p className='text-xl text-black font-semibold'>{props.title}</p>
+        </div>
     )
 }
 
@@ -55,48 +93,14 @@ const sidebarInfo = [
 const sidebarFooterInfo = [
     {
         title: 'Light UI',
-        icon: <SunMoon size={20} color='white' className='mr-2'/>
+        icon: <SunMoon size={20} color='black' className='mr-2'/>
     },
     {
         title: 'Settings',
-        icon: <Settings size={20} color='white' className='mr-2'/>
+        icon: <Settings size={20} color='black' className='mr-2'/>
     },
     {
         title: 'Support',
-        icon: <FileQuestion size={20} color='white' className='mr-2'/>
+        icon: <FileQuestion size={20} color='black' className='mr-2'/>
     },
 ]
-
-export default function Sidebar() {
-  return (
-    <div className='bg-gray-800 w-[200px] h-full flex flex-col'>
-        <div className='w-full h-12 flex items-center justify-left bg-transparent border-none p-4 mt-4 mb-2'>
-            <Image src={'/chess.png'} alt='image' width={32} height={32} className='relative z-10'/>
-            <p className='text-3xl text-white font-bold absolute left-8 top-3 text-emerald-400'>GAMBITAI</p>
-        </div>
-
-        {sidebarInfo.map((item, index) => (
-            <SidebarButton key={index} title={item.title} icon={item.icon}/>
-        ))}
-
-        <div className="w-full h-14 flex items-center justify-left bg-transparent border-none px-6 mb-2 hover:bg-gray-900">
-            <Ellipsis size={32} color='white' className='mr-4'/>
-            <p className='text-xl text-white font-normal'>more</p>
-        </div>
-
-        <div className='px-4 mb-4'>
-            <Input type="text" placeholder="Search" className="h-10"/>
-        </div>
-
-        <div className='mt-auto mb-4'>
-            {sidebarFooterInfo.map((item, index) => (
-                <Button key={index} className="w-full bg-transparent flex justify-start	items-center px-6 hover:bg-transparent">
-                    {item.icon}
-                    <p className='text-white text-md font-semibold'>{item.title}</p>
-                </Button>
-            ))}
-            {/* <p className='text-white text-xs text-center'>© 2021 GambitAi</p> */}
-        </div>
-    </div>
-  )
-}
